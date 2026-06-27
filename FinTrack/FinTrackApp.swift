10554ebec555913602c4263935f804d5262e90c8
DiliniKB <dilini123bandara@gmail.com>
2026-06-27 19:05:32 +0530

    Initial Commit

---
+++ FinTrack/FinTrackApp.swift
@@ -0,0 +1,32 @@
+//
+//  FinTrackApp.swift
+//  FinTrack
+//
+//  Created by Dilini Bandara on 2026-06-27.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct FinTrackApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
